0ae4afe747019f89cbc9f72eb060c1221fe101f9
jiahaoluo <luojiahao5@huawei.com>
2022-02-24 14:49:32 +0800

    add umask protectection when preparing dirs
    Signed-off-by: jiahaoluo <luojiahao5@huawei.com>

--- services/storage_daemon/utils/file_utils.cpp
+++ services/storage_daemon/utils/file_utils.cpp
@@ -137,10 +137,12 @@ bool PrepareDir(const std::string &path, mode_t mode, uid_t uid, gid_t gid)
         return true;
     }
 
+    mode_t mask = umask(0);
     if (MkDir(path, mode)) {
         LOGE("failed to mkdir, errno %{public}d", errno);
         return false;
     }
+    umask(mask);
 
     if (ChOwn(path, uid, gid)) {
         LOGE("failed to chown, errno %{public}d", errno);
