f369cd42a55c65000ae630b3e6be1fecf873479f
linjun9528 <linjun27@huawei.com>
2022-03-03 15:41:30 +0800

    add storage daemon volume test code
    Signed-off-by: linjun9528 <linjun27@huawei.com>

--- services/storage_daemon/volume/test/external_volume_info_test.cpp
+++ services/storage_daemon/volume/test/external_volume_info_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +16,6 @@
 #include <gtest/gtest.h>
 
 #include <linux/kdev_t.h>
-#include <sys/types.h>
 
 #include "storage_service_errno.h"
 #include "storage_service_log.h"
