700a9d832fb67143d41ab47291bb3e13691725c1
张文迪 <zhangwendi3@huawei.com>
2022-03-12 14:56:45 +0800

    add permission check
    Signed-off-by: 张文迪 <zhangwendi3@huawei.com>

--- services/storage_daemon/ipc/src/storage_daemon_stub.cpp
+++ services/storage_daemon/ipc/src/storage_daemon_stub.cpp
@@ -14,11 +14,52 @@
  */
 
 #include "ipc/storage_daemon_stub.h"
+#include "accesstoken_kit.h"
+#include "ipc_skeleton.h"
+#include "native_token_info.h"
+#include "string_ex.h"
 #include "storage_service_errno.h"
 #include "storage_service_log.h"
 
 namespace OHOS {
 namespace StorageDaemon {
+static bool GetClientUid(int &uid)
+{
+    uid = IPCSkeleton::GetCallingUid();
+    return true;
+}
+
+static bool GetClientTokenId(int &TokenId)
+{
+    TokenId = IPCSkeleton::GetCallingTokenID();;
+    return true;
+}
+
+static std::string GetProcessName(int pid) {
+    Security::AccessToken::NativeTokenInfo tokenInfo = Security::AccessToken::NativeTokenInfo();
+    Security::AccessToken::AccessTokenKit::GetNativeTokenInfo(pid, tokenInfo);
+    return tokenInfo.processName;
+}
+
+bool CheckClientPermission() {
+    int uid = 0;
+    int tokenId = 0;
+    if (!GetClientUid(uid)) {
+        LOGE("GetClientUid: fail");
+    }
+
+    if (!GetClientTokenId(tokenId)) {
+        LOGE("GetClientTokenId: fail");
+    }
+
+    std::string clientName = GetProcessName(tokenId);
+    LOGI("GetClientProcessName:%{public}s", clientName.c_str());
+    if (IsSameTextStr(clientName, "storage_manager") || uid == 0) {
+        return true;
+    }
+    return false;
+}
+
 int32_t StorageDaemonStub::OnRemoteRequest(uint32_t code, MessageParcel &data,
                                            MessageParcel &reply, MessageOption &option)
 {
@@ -26,7 +67,11 @@ int32_t StorageDaemonStub::OnRemoteRequest(uint32_t code, MessageParcel &data,
     if (GetDescriptor() != remoteDescriptor) {
         return E_PERMISSION_DENIED;
     }
-
+    
+    if (!CheckClientPermission()) {
+        LOGE("StorageDaemon checkPermission error");
+        return E_PERMISSION_DENIED;
+    }
     LOGI("recv remote request code %{public}d", code);
     int err = E_OK;
     switch (code) {
