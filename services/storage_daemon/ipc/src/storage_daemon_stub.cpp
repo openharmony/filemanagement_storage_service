0e7f05d807882c45466a56b35561ec5389f71cce
张文迪 <zhangwendi3@huawei.com>
2022-03-15 17:45:04 +0800

    code revise
    Signed-off-by: 张文迪 <zhangwendi3@huawei.com>

--- services/storage_daemon/ipc/src/storage_daemon_stub.cpp
+++ services/storage_daemon/ipc/src/storage_daemon_stub.cpp
@@ -56,7 +56,7 @@ bool CheckClientPermission()
 
     std::string clientName = GetProcessName(tokenId);
     LOGI("GetClientProcessName:%{public}s", clientName.c_str());
-    if (clientName == "storage_manager") || uid == 0) {
+    if (clientName == "storage_manager" || uid == 0) {
         return true;
     }
     return false;
