6ef00d2f303f2469a1516b4826bbd2cda6dcc5f9
fangzhiyi18 <fangzhiyi1@huawei.com>
2023-12-02 07:00:05 +0000

    告警修改
    Signed-off-by: fangzhiyi18 <fangzhiyi1@huawei.com>

--- services/storage_manager/include/utils/storage_utils.h
+++ services/storage_manager/include/utils/storage_utils.h
@@ -21,7 +21,7 @@ namespace OHOS {
 namespace StorageManager {
 static const int64_t UNIT = 1000;
 static const int64_t THRESHOLD = 512;
-static const int64_t ONE_GB = 10000000000;
+static const int64_t ONE_GB = 1000000000;
 int64_t GetRoundSize(int64_t size);
 } // namespace STORAGE_MANAGER
 } // namespace OHOS
