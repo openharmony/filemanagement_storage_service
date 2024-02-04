3db48735ea6758a1e6b1b71e2552fe58c3748aa6
yang-jingbo1985 <yangjingbo10@huawei.com>
2024-02-04 17:23:18 +0800

    增量备份获取应用数据接口TDD Signed-off-by: yangjingbo10 <yangjingbo10@huawei.com>
    Change-Id: If55bbf45fe61163a8b33d6aaae6fa109265504f3

--- services/storage_daemon/ipc/test/storage_daemon_test.cpp
+++ services/storage_daemon/ipc/test/storage_daemon_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,9 @@
  */
 
 #include <fcntl.h>
+#include <fstream>
 #include <gtest/gtest.h>
+#include <iostream>
 #include <mntent.h>
 #include <unordered_map>
 
@@ -425,6 +427,70 @@ HWTEST_F(StorageDaemonTest, Storage_Manager_StorageDaemonTest_SetBundleQuota_001
     auto ret = storageDaemon_->SetBundleQuota(bundleName, uid, bundleDataDirPath, limitSizeMb);
     EXPECT_TRUE(ret == E_SYS_CALL);
     GTEST_LOG_(INFO) << "Storage_Manager_StorageDaemonTest_SetBundleQuota_001 end";
+}
+
+/**
+ * @tc.name: Storage_Manager_StorageDaemonTest_GetBundleStatsForIncrease_001
+ * @tc.desc: check the GetBundleStatsForIncrease function
+ * @tc.type: FUNC
+ * @tc.require: AR000IGCR7
+ */
+HWTEST_F(StorageDaemonTest, Storage_Manager_StorageDaemonTest_GetBundleStatsForIncrease_001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_Manager_StorageDaemonTest_GetBundleStatsForIncrease_001 start";
+
+    ASSERT_TRUE(storageDaemon_ != nullptr);
+    // create parameter
+    uint32_t userId = 100;
+    std::vector<std::string> bundleNames;
+    std::string bundleName = "com.ohos.UserFile.ExternalFileManager";
+    bundleNames.emplace_back(bundleName);
+    std::vector<int64_t> incrementalBackTimes;
+    int64_t lastIncrementalTime = 0;
+    incrementalBackTimes.emplace_back(lastIncrementalTime);
+    std::vector<int64_t> pkgFileSizes;
+    // prepare config file
+    mode_t mode = 0771;
+    // backup_sa bundle path
+    std::string backupSaBundleDir = BACKUP_PATH_PREFIX + std::to_string(userId) + BACKUP_PATH_SURFFIX + bundleName +
+        FILE_SEPARATOR_CHAR;
+    if (!StorageTest::StorageTestUtils::CheckDir(backupSaBundleDir)) {
+        StorageTest::StorageTestUtils::MkDir(backupSaBundleDir, mode);
+    }
+    // backup_sa include/exclude
+    std::string incExFilePath = backupSaBundleDir + BACKUP_INCEXC_SYMBOL + std::to_string(lastIncrementalTime);
+    std::ofstream incExcFile;
+    incExcFile.open(incExFilePath.data(), std::ios::out | std::ios::trunc);
+    if (!incExcFile.is_open()) {
+        EXPECT_TRUE(false) << "Failed to create file for include-exclude config";
+    }
+    incExcFile << BACKUP_INCLUDE << std::endl;
+    std::string include = "/storage/Users/currentUser/";
+    incExcFile << include << std::endl;
+
+    incExcFile << BACKUP_EXCLUDE << std::endl;
+    std::string exclude = "/storage/Users/currentUser/.Trash/";
+    incExcFile << exclude << std::endl;
+    incExcFile.close();
+
+    // backup_sa stat
+    std::string statFilePath = backupSaBundleDir + BACKUP_STAT_SYMBOL + std::to_string(lastIncrementalTime);
+    std::ofstream statFile;
+    statFile.open(statFilePath.data(), std::ios::out | std::ios::trunc);
+    if (!statFile.is_open()) {
+        EXPECT_TRUE(false) << "Failed to create file for bundle stat";
+    }
+    statFile.close();
+    // invoke GetBundleStatsForIncrease
+    auto ret = storageDaemon_->GetBundleStatsForIncrease(userId, bundleNames, incrementalBackTimes, pkgFileSizes);
+    EXPECT_TRUE(ret == E_OK);
+    EXPECT_GE(pkgFileSizes[0], 0);
+
+    if (StorageTest::StorageTestUtils::CheckDir(backupSaBundleDir)) {
+        StorageTest::StorageTestUtils::RmDirRecurse(backupSaBundleDir);
+    }
+
+    GTEST_LOG_(INFO) << "Storage_Manager_StorageDaemonTest_GetBundleStatsForIncrease_001 end";
 }
 } // STORAGE_DAEMON
 } // OHOS
