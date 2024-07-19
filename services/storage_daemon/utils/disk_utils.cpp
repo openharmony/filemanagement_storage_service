4757ed2803941faa3ed1c26ea1ce0af3bebde2b3
sunxiaoqiang <sunxiaoqiang7@huawei.com>
2024-07-19 06:14:04 +0000

    update services/storage_daemon/utils/disk_utils.cpp.
    Signed-off-by: sunxiaoqiang <sunxiaoqiang7@huawei.com>

--- services/storage_daemon/utils/disk_utils.cpp
+++ services/storage_daemon/utils/disk_utils.cpp
@@ -89,7 +89,6 @@ int32_t ReadMetadata(const std::string &devPath, std::string &uuid, std::string 
     uuid = GetBlkidData(devPath, "UUID");
     type = GetBlkidData(devPath, "TYPE");
     label = GetBlkidData(devPath, "LABEL");
-    
     LOGI("ReadMetadata, fsUuid=%{public}s, fsType=%{public}s, fsLabel=%{public}s.", GetAnonyString(uuid).c_str(),
         type.c_str(), label.c_str());
     if (uuid.empty() || type.empty()) {
