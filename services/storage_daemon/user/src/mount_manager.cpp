2ca805156cbeac29e6eb378fe0a07ab7d39a00b4
Cai Xincheng <caixincheng@huawei.com>
2024-08-02 12:05:50 +0800

    add asset system service dir
    Signed-off-by: Cai Xincheng <caixincheng@huawei.com>
    Change-Id: Ia71445a413c529f312e5199bb2e146329d05ece7

--- services/storage_daemon/user/src/mount_manager.cpp
+++ services/storage_daemon/user/src/mount_manager.cpp
@@ -124,6 +124,7 @@ MountManager::MountManager()
                   {"/data/service/el2/%d/hwid_service", MODE_0711, OID_HWID, OID_HWID},
                   {"/data/service/el2/%d/huks_service", MODE_0711, OID_HUKS, OID_HUKS},
                   {"/data/service/el4/%d/huks_service", MODE_0711, OID_HUKS, OID_HUKS},
+                  {"/data/service/el2/%d/asset_service", MODE_0711, OID_ASSET, OID_ASSET},
                   {"/data/service/el2/%d/dlp_credential_service", MODE_0711, OID_DLP_CREDENTIAL, OID_DLP_CREDENTIAL}},
       fileManagerDir_{{"/data/service/el2/%d/hmdfs/account/files/Docs", MODE_02771, OID_FILE_MANAGER, OID_FILE_MANAGER},
                    {"/data/service/el2/%d/hmdfs/account/files/Docs/Documents",
