c001a3fda54cc72336692a5139c3514068380678
lianwei <lianwei3@huawei.com>
2024-08-05 15:14:39 +0800

    storage_service:补充UT用例
    Signed-off-by: lianwei <lianwei3@huawei.com>

--- services/storage_daemon/client/test/storage_daemon_client_test.cpp
+++ services/storage_daemon/client/test/storage_daemon_client_test.cpp
@@ -135,7 +135,7 @@ HWTEST_F(StorageDaemonClientTest, Storage_Service_StorageDaemonClientTest_MountD
     std::string networkId = "testnetworkid";
     std::string deviceId = "testdevid";
     int32_t ret = storageDaemonClient_->MountDfsDocs(userId, relativePath, networkId, deviceId);
-    EXPECT_EQ(ret, E_PREPARE_DIR);
+    EXPECT_EQ(ret, E_MOUNT);
     GTEST_LOG_(INFO) << "Storage_Service_StorageDaemonClientTest_MountDfsDocs_001 end";
 }
 
