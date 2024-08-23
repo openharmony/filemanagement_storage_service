23885d90b516312b6a0f7167954f7198c57da92b
lixiyuan <lixiyuan7@huawei.com>
2024-08-23 02:22:38 +0000

    update services/storage_daemon/libfscrypt/src/sysparam_static.c.
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- services/storage_daemon/libfscrypt/src/sysparam_static.c
+++ services/storage_daemon/libfscrypt/src/sysparam_static.c
@@ -29,7 +29,7 @@ int GetFscryptParameter(const char *key, const char *def, char *value, unsigned 
 int SetFscryptParameter(const char *policy, const char *value)
 {
     if (!policy || !value) {
-        FSCRYPT_LOGE("SetFscryptParameter: there is null param");
+        LOGE("SetFscryptParameter: there is null param");
         return -EINVAL;
     }
     return SystemWriteParam(policy, value);
