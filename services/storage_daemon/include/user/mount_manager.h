039ebc4792985525dd0f427e0e9e4a89f4032d48
姚子鹏 <yaozipeng1@huawei.com>
2024-09-02 16:27:09 +0800

    file
    Signed-off-by: 姚子鹏 <yaozipeng1@huawei.com>

--- services/storage_daemon/include/user/mount_manager.h
+++ services/storage_daemon/include/user/mount_manager.h
@@ -53,6 +53,7 @@ constexpr uid_t OID_DDMS = 3012;
 constexpr uid_t OID_HWID = 7008;
 constexpr uid_t OID_DLP_CREDENTIAL = 3553;
 constexpr uid_t OID_ACCOUNT = 3058;
+constexpr uid_t OID_PARENT_CONTROL = 7007;
 constexpr uid_t USER_ID_BASE = 200000;
 
 class MountManager final {
