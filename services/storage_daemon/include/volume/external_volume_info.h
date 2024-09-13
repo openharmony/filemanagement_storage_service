fe9cf7788468d11ab39029538b8559e1c065d889
s30051537 <sunxiaoqiang7@huawei.com>
2024-09-13 17:44:36 +0800

    f2fs 挂载
    Signed-off-by: s30051537 <sunxiaoqiang7@huawei.com>

--- services/storage_daemon/include/volume/external_volume_info.h
+++ services/storage_daemon/include/volume/external_volume_info.h
@@ -54,11 +54,12 @@ private:
 
     const std::string devPathDir_ = "/dev/block/%s";
     const std::string mountPathDir_ = "/mnt/data/external/%s";
-    std::vector<std::string> supportMountType_ = { "ext2", "ext3", "ext4", "ntfs", "exfat", "vfat", "hmfs" };
+    std::vector<std::string> supportMountType_ = { "ext2", "ext3", "ext4", "ntfs", "exfat", "vfat", "hmfs", "f2fs" };
     std::map<std::string, std::string> supportFormatType_ = {{"exfat", "mkfs.exfat"}, {"vfat", "newfs_msdos"}};
 
     int32_t ReadMetadata();
     int32_t DoMount4Ext(uint32_t mountFlags);
+    int32_t DoMount4Hmfs(uint32_t mountFlags);
     int32_t DoMount4Ntfs(uint32_t mountFlags);
     int32_t DoMount4Exfat(uint32_t mountFlags);
     int32_t DoMount4OtherType(uint32_t mountFlags);
