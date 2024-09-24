e15f7d30c34b4b3e036606e9956b874375e3efd4
zhangchenyang <zhangchenyang30@huawei.com>
2024-09-24 17:07:45 +0800

    【5.0.1-Release】【storage_service】修改不支持el5时不调用内核问题
    Signed-off-by: zhangchenyang <zhangchenyang30@huawei.com>

--- services/storage_daemon/crypto/src/key_manager.cpp
+++ services/storage_daemon/crypto/src/key_manager.cpp
@@ -1509,16 +1509,7 @@ bool KeyManager::IsUeceSupport()
 
 int KeyManager::IsUeceSupportWithErrno()
 {
-    FILE *f = fopen(UECE_PATH, "r+");
-    if (f == nullptr) {
-        if (errno == ENOENT) {
-            LOGE("uece does not support !");
-            return ENOENT;
-        }
-        LOGE("open uece failed, errno : %{public}d", errno);
-        return errno;
-    }
-    int fd = fileno(f);
+    int fd = open(UECE_PATH, O_RDWR);
     if (fd < 0) {
         if (errno == ENOENT) {
             LOGE("uece does not support !");
@@ -1527,7 +1518,7 @@ int KeyManager::IsUeceSupportWithErrno()
         LOGE("open uece failed, errno : %{public}d", errno);
         return errno;
     }
-    (void)fclose(f);
+    close(fd);
     LOGI("uece is support.");
     return E_OK;
 }
