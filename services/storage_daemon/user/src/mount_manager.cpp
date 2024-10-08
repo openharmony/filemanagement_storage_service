f7e3d2c5054060b8fecdb4c43e0d6541c2087475
zhangchenyang <zhangchenyang30@huawei.com>
2024-10-08 18:42:15 +0800

    【master】【storage_service】startUser接口可重入加固
    Signed-off-by: zhangchenyang <zhangchenyang30@huawei.com>

--- services/storage_daemon/user/src/mount_manager.cpp
+++ services/storage_daemon/user/src/mount_manager.cpp
@@ -530,8 +530,8 @@ int32_t MountManager::CloudTwiceMount(int32_t userId)
             ret = mountRet;
         }
     }
-    if (IsPathMounted(cloudPath)) {
-        LOGI("path has mounted, %{public}s", cloudPath.c_str());
+    if (IsPathMounted(cloudMediaPath)) {
+        LOGI("path has mounted, %{public}s", cloudMediaPath.c_str());
     } else {
         mountRet = CloudMount(userId, cloudMediaPath);
         if (mountRet != E_OK) {
