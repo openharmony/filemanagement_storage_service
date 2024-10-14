85d2f3123ee48982e193ec3f681dbbb871617e85
李尚 <lishang21@huawei.com>
2024-10-14 03:25:41 +0000

    update services/storage_daemon/include/mtp/mtp_device_manager.h.
    Signed-off-by: 李尚 <lishang21@huawei.com>

--- services/storage_daemon/include/mtp/mtp_device_manager.h
+++ services/storage_daemon/include/mtp/mtp_device_manager.h
@@ -27,7 +27,7 @@ constexpr int32_t DEFAULT_DEV_INDEX = 1;
 constexpr uid_t FILE_MANAGER_UID = 1006;
 constexpr gid_t FILE_MANAGER_GID = 1006;
 constexpr mode_t PUBLIC_DIR_MODE = 02770;
- 
+
 struct MtpDeviceInfo {
     std::string id;
     std::string uuid;
@@ -39,10 +39,10 @@ struct MtpDeviceInfo {
     uint16_t vendorId;
     uint16_t productId;
 };
- 
+
 class MtpDeviceManager : public NoCopyable  {
     DECLARE_DELAYED_SINGLETON(MtpDeviceManager);
- 
+
 public:
     int32_t MountDevice(const MtpDeviceInfo &device);
     int32_t UmountDevice(const MtpDeviceInfo &device, bool needNotify);
