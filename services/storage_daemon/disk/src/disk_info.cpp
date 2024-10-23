4148c982cd7934a2a7de150392818d42220432d9
s30051537 <sunxiaoqiang7@huawei.com>
2024-10-23 11:27:13 +0800

    修改MBR分区表类型挂载
    Signed-off-by: s30051537 <sunxiaoqiang7@huawei.com>

--- services/storage_daemon/disk/src/disk_info.cpp
+++ services/storage_daemon/disk/src/disk_info.cpp
@@ -228,8 +228,8 @@ int DiskInfo::ReadPartition()
 
 bool DiskInfo::CreateMBRVolume(int32_t type, dev_t dev)
 {
-    // FAT16 || NTFS/EXFAT || W95 FAT32 || W95 FAT32 || W95 FAT16 || EFI FAT32 || EXT 2/3/4
-    if (type == 0x06 || type == 0x07 || type == 0x0b || type == 0x0c || type == 0x0e || type == 0x1b || type == 0x83) {
+    // FAT16 || NTFS/EXFAT || W95 FAT32 || W95 FAT32 || W95 FAT16 || EXT 2/3/4
+    if (type == 0x06 || type == 0x07 || type == 0x0b || type == 0x0c || type == 0x0e || type == 0x83) {
         if (CreateVolume(dev) == E_OK) {
             return true;
         }
@@ -308,7 +308,10 @@ void DiskInfo::ProcessPartition(std::vector<std::string>::iterator &it, const st
             return;
         }
         int32_t type = std::stoi("0x0" + *it, 0, 16);
-        foundPart = CreateMBRVolume(type, partitionDev);
+        if (!CreateMBRVolume(type, partitionDev)) {
+            LOGE("Create MBR Volume failed");
+        }
+        foundPart = true;
     } else if (table == Table::GPT) {
         if (CreateVolume(partitionDev) == E_OK) {
             foundPart = true;
