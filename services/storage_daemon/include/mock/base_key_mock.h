742d9aae2566d8e7b719fbe77f384b03e61dd75e
dengrenqi <1069093439@qq.com>
2024-11-11 15:08:49 +0800

    函数优化
    Signed-off-by: dengrenqi <1069093439@qq.com>

--- services/storage_daemon/include/mock/base_key_mock.h
+++ services/storage_daemon/include/mock/base_key_mock.h
@@ -39,7 +39,7 @@ public:
     virtual bool GetOriginKey(KeyBlob &originKey) = 0;
     virtual bool SaveKeyBlob(const KeyBlob &blob, const std::string &path) = 0;
     virtual bool LoadKeyBlob(KeyBlob &blob, const std::string &path, const uint32_t size) = 0;
-    virtual bool KeyDesclsEmpty() = 0;
+    virtual bool KeyDescIsEmpty() = 0;
 public:
     static inline std::shared_ptr<IBaseKeyMoc> baseKeyMoc = nullptr;
 };
@@ -53,7 +53,7 @@ public:
     MOCK_METHOD1(UpdateKey, bool(const std::string &keypath));
     MOCK_METHOD1(RestoreKey, bool(const UserAuth &auth));
     MOCK_METHOD0(UpgradeKeys, bool());
-    MOCK_METHOD0(KeyDesclsEmpty, bool());
+    MOCK_METHOD0(KeyDescIsEmpty, bool());
     MOCK_METHOD4(DecryptKeyBlob, bool(const UserAuth &, const std::string &, KeyBlob &, KeyBlob &));
     MOCK_METHOD4(EncryptKeyBlob, bool(const UserAuth &, const std::string &, KeyBlob &, KeyBlob &));
     MOCK_METHOD1(RenameKeyPath, bool(const std::string &keypath));
