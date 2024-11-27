5241e7a98a83bd296bc57a7438be95edcaad4c4c
李尚 <lishang21@huawei.com>
2024-11-27 08:44:06 +0000

    update services/storage_daemon/include/crypto/fbex.h.
    Signed-off-by: 李尚 <lishang21@huawei.com>

--- services/storage_daemon/include/crypto/fbex.h
+++ services/storage_daemon/include/crypto/fbex.h
@@ -67,7 +67,7 @@ public:
     static int GenerateAppkey(UserIdToFbeStr &userIdToFbe, uint32_t hashId, std::unique_ptr<uint8_t[]> &keyId,
                               uint32_t size);
     static int LockUece(uint32_t userIdSingle, uint32_t userIdDouble, bool &isFbeSupport);
-    
+    static uint32_t GetBufferSize(uint32_t status);
 };
 } // namespace StorageDaemon
 } // namespace OHOS
