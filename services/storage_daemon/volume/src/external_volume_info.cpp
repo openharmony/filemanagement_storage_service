8b8160bf081e7da2555338c85b4991f4348eac7f
sunxiaoqiang <sunxiaoqiang7@huawei.com>
2024-12-16 08:04:24 +0000

    update services/storage_daemon/volume/src/external_volume_info.cpp.
    Signed-off-by: sunxiaoqiang <sunxiaoqiang7@huawei.com>

--- services/storage_daemon/volume/src/external_volume_info.cpp
+++ services/storage_daemon/volume/src/external_volume_info.cpp
@@ -36,7 +36,7 @@
 using namespace std;
 namespace OHOS {
 namespace StorageDaemon {
-constexpr int32_t WAIT_THREAD_TIMEOUT_S = 12;
+constexpr int32_t WAIT_THREAD_TIMEOUT_S = 15;
 int32_t ExternalVolumeInfo::ReadMetadata()
 {
     int32_t ret = OHOS::StorageDaemon::ReadMetadata(devPath_, fsUuid_, fsType_, fsLabel_);
