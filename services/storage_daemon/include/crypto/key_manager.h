9432123090f92747608ebdd92c9d72e6c12b8e14
zhaoshuyuan <zhaoshuyuan2@huawei.com>
2025-01-09 13:17:35 +0000

    update services/storage_daemon/include/crypto/key_manager.h.
    Signed-off-by: zhaoshuyuan <zhaoshuyuan2@huawei.com>

--- services/storage_daemon/include/crypto/key_manager.h
+++ services/storage_daemon/include/crypto/key_manager.h
@@ -167,6 +167,7 @@ private:
     bool HashElxActived(unsigned int user, KeyType type);
     bool HasElxDesc(std::map<unsigned int, std::shared_ptr<BaseKey>> &userElKey_, KeyType type, unsigned int user);
     bool IsAppCloneUser(unsigned int user);
+    int CheckNeedRestoreVersion(unsigned int user, KeyType type);
 #ifdef EL5_FILEKEY_MANAGER
     int GenerateAndLoadAppKeyInfo(uint32_t userId, const std::vector<std::pair<int, std::string>> &keyInfo);
 #endif
