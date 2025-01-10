50526549843faf1fde2e77bf42a7cce0b93bc7db
lvyuanyuan <lvyuanyuan7@huawei.com>
2025-01-10 17:14:47 +0800

    sync code
    Signed-off-by: lvyuanyuan <lvyuanyuan7@huawei.com>
    Change-Id: I4142cf3d6213ec9e91726d0c79a28ff0c87a0066

--- services/storage_manager/ipc/test/storage_manager_stub_test.cpp
+++ services/storage_manager/ipc/test/storage_manager_stub_test.cpp
@@ -31,48 +31,6 @@ using namespace testing::ext;
 
 namespace {
     const int ERROR_CODE = 99999;
-    int32_t g_code[] = {
-        static_cast<int32_t>(StorageManagerInterfaceCode::PREPARE_ADD_USER),
-        static_cast<int32_t>(StorageManagerInterfaceCode::REMOVE_USER),
-        static_cast<int32_t>(StorageManagerInterfaceCode::PREPARE_START_USER),
-        static_cast<int32_t>(StorageManagerInterfaceCode::STOP_USER),
-        static_cast<int32_t>(StorageManagerInterfaceCode::NOTIFY_VOLUME_CREATED),
-        static_cast<int32_t>(StorageManagerInterfaceCode::NOTIFY_VOLUME_MOUNTED),
-        static_cast<int32_t>(StorageManagerInterfaceCode::NOTIFY_VOLUME_STATE_CHANGED),
-        static_cast<int32_t>(StorageManagerInterfaceCode::MOUNT),
-        static_cast<int32_t>(StorageManagerInterfaceCode::UNMOUNT),
-        static_cast<int32_t>(StorageManagerInterfaceCode::NOTIFY_DISK_CREATED),
-        static_cast<int32_t>(StorageManagerInterfaceCode::NOTIFY_DISK_DESTROYED),
-        static_cast<int32_t>(StorageManagerInterfaceCode::PARTITION),
-        static_cast<int32_t>(StorageManagerInterfaceCode::CREATE_USER_KEYS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::DELETE_USER_KEYS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::UPDATE_USER_AUTH),
-        static_cast<int32_t>(StorageManagerInterfaceCode::ACTIVE_USER_KEY),
-        static_cast<int32_t>(StorageManagerInterfaceCode::INACTIVE_USER_KEY),
-        static_cast<int32_t>(StorageManagerInterfaceCode::UPDATE_KEY_CONTEXT),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_VOL_BY_UUID),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_VOL_BY_ID),
-        static_cast<int32_t>(StorageManagerInterfaceCode::SET_VOL_DESC),
-        static_cast<int32_t>(StorageManagerInterfaceCode::FORMAT),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_DISK_BY_ID),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_TOTAL),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_FREE),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_SYSTEM_SIZE),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_TOTAL_SIZE),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_FREE_SIZE),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_BUNDLE_STATUS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_CURR_BUNDLE_STATS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_CURR_USER_STATS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_USER_STATS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_ALL_VOLUMES),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GET_ALL_DISKS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::LOCK_USER_SCREEN),
-        static_cast<int32_t>(StorageManagerInterfaceCode::UNLOCK_USER_SCREEN),
-        static_cast<int32_t>(StorageManagerInterfaceCode::LOCK_SCREEN_STATUS),
-        static_cast<int32_t>(StorageManagerInterfaceCode::SET_BUNDLE_QUOTA),
-        static_cast<int32_t>(StorageManagerInterfaceCode::GENERATE_APP_KEY),
-        static_cast<int32_t>(StorageManagerInterfaceCode::DELETE_APP_KEY),
-    };
 }
 
 class StorageManagerStubTest : public testing::Test {
@@ -139,82 +97,6 @@ HWTEST_F(StorageManagerStubTest, Storage_Manager_StorageManagerStubTest_OnRemote
     EXPECT_TRUE(ret != E_OK) << "request code error";
 
     GTEST_LOG_(INFO) << "Storage_Manager_StorageManagerStubTest_OnRemoteRequest_002 end";
-}
-
-/**
- * @tc.name: Storage_Manager_StorageManagerStubTest_OnRemoteRequest_003
- * @tc.desc: Verify the OnRemoteRequest function.
- * @tc.type: FUNC
- * @tc.require: AR000GK4HB
- */
-HWTEST_F(StorageManagerStubTest, Storage_Manager_StorageManagerStubTest_OnRemoteRequest_003, TestSize.Level1)
-{
-    GTEST_LOG_(INFO) << "Storage_Manager_StorageManagerStubTest_OnRemoteRequest_003 start";
-
-    StorageManagerStubMock mock;
-    std::vector<string> perms;
-    perms.push_back("ohos.permission.STORAGE_MANAGER");
-    perms.push_back("ohos.permission.STORAGE_MANAGER_CRYPT");
-    perms.push_back("ohos.permission.MOUNT_UNMOUNT_MANAGER");
-    perms.push_back("ohos.permission.MOUNT_FORMAT_MANAGER");
-    uint64_t tokenId = 0;
-    PermissionUtilsTest::SetAccessTokenPermission("StorageManagerPxyTest", perms, tokenId);
-    ASSERT_TRUE(tokenId != 0);
-    EXPECT_CALL(mock, PrepareAddUser(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, RemoveUser(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, PrepareStartUser(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, StopUser(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, NotifyVolumeCreated(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, NotifyVolumeMounted(testing::_, testing::_, testing::_, testing::_, testing::_))
-        .WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, NotifyVolumeStateChanged(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, Mount(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, Unmount(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, NotifyDiskCreated(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, NotifyDiskDestroyed(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, Partition(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetVolumeByUuid(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetVolumeById(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, SetVolumeDescription(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, Format(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetDiskById(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GenerateUserKeys(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, DeleteUserKeys(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, UpdateUserAuth(testing::_, testing::_, testing::_, testing::_, testing::_))
-        .WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, ActiveUserKey(testing::_, testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, InactiveUserKey(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, UpdateKeyContext(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetFreeSizeOfVolume(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetTotalSizeOfVolume(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetSystemSize(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetTotalSize(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetFreeSize(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetBundleStats(testing::_, testing::_, testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetCurrentBundleStats(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetUserStorageStats(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetUserStorageStats(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetAllVolumes(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetAllDisks(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, LockUserScreen(testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, UnlockUserScreen(testing::_, testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GetLockScreenStatus(testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, SetBundleQuota(testing::_, testing::_, testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, GenerateAppkey(testing::_, testing::_, testing::_)).WillOnce(testing::Return(E_OK));
-    EXPECT_CALL(mock, DeleteAppkey(testing::_)).WillOnce(testing::Return(E_OK));
-
-    for (auto c : g_code) {
-        MessageParcel data;
-        MessageParcel reply;
-        MessageOption option(MessageOption::TF_SYNC);
-        bool bRet = data.WriteInterfaceToken(StorageManagerProxy::GetDescriptor());
-        EXPECT_TRUE(bRet) << "write token error";
-        int32_t ret = mock.OnRemoteRequest(c, data, reply, option);
-        EXPECT_TRUE(ret == E_OK);
-        EXPECT_TRUE(reply.ReadInt32() == E_OK);
-    }
-
-    GTEST_LOG_(INFO) << "Storage_Manager_StorageManagerStubTest_OnRemoteRequest_003 end";
 }
 } // STORAGE_MANAGER
 } // OHOS
