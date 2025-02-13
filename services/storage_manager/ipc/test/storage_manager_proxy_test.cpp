efee674bd9024a2eeedb09413c4ab2b3a6bdb728
wangpggg <wangpeng477@huawei.com>
2025-02-13 14:10:25 +0800

    add ut
    Signed-off-by: wangpeng <wangpeng477@huawei.com>

--- services/storage_manager/ipc/test/storage_manager_proxy_test.cpp
+++ services/storage_manager/ipc/test/storage_manager_proxy_test.cpp
@@ -1772,4 +1772,33 @@ HWTEST_F(StorageManagerProxyTest, Storage_manager_proxy_UMountMediaFuse_0000, te
     #endif
     GTEST_LOG_(INFO) << "StorageManagerProxyTest-end Storage_manager_proxy_UMountMediaFuse_0000";
 }
+
+/**
+ * @tc.number: SUB_STORAGE_Storage_manager_proxy_GetUserNeedActiveStatus_0000
+ * @tc.name: Storage_manager_proxy_GetUserNeedActiveStatus_0000
+ * @tc.desc: Test function of GetUserNeedActiveStatus interface for SUCCESS.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ */
+HWTEST_F(StorageManagerProxyTest, Storage_manager_proxy_GetUserNeedActiveStatus_0000, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "StorageManagerProxyTest-begin Storage_manager_proxy_GetUserNeedActiveStatus_0000";
+    ASSERT_TRUE(mock_ != nullptr) << "StorageManagerServiceMock failed";
+    EXPECT_CALL(*mock_, SendRequest(testing::_, testing::_, testing::_, testing::_))
+        .Times(1)
+        .WillOnce(testing::Invoke(mock_.GetRefPtr(), &StorageManagerServiceMock::InvokeSendRequest));
+    uint32_t userId = 120;
+    bool needActive = false;
+    ASSERT_TRUE(proxy_ != nullptr) << "StorageManagerProxy failed";
+    uint32_t result = proxy_->GetUserNeedActiveStatus(userId, needActive);
+    EXPECT_EQ(result, E_OK);
+
+    EXPECT_CALL(*mock_, SendRequest(testing::_, testing::_, testing::_, testing::_))
+        .Times(1)
+        .WillOnce(testing::Return(E_WRITE_PARCEL_ERR));
+    result = proxy_->GetUserNeedActiveStatus(userId, needActive);
+    EXPECT_EQ(result, E_WRITE_PARCEL_ERR);
+    GTEST_LOG_(INFO) << "StorageManagerProxyTest-end Storage_manager_proxy_GetUserNeedActiveStatus_0000";
+}
 } // namespace
