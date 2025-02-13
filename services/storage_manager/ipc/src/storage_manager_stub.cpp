6e8a15365067141a160289e6422f68578fd771ba
lixiyuan <lixiyuan7@huawei.com>
2025-02-14 00:18:58 +0800

    无密码单升单修复
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- services/storage_manager/ipc/src/storage_manager_stub.cpp
+++ services/storage_manager/ipc/src/storage_manager_stub.cpp
@@ -295,7 +295,8 @@ int32_t StorageManagerStub::OnRemoteRequest(uint32_t code,
 
 int32_t StorageManagerStub::HandlePrepareAddUser(MessageParcel &data, MessageParcel &reply)
 {
-    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT)) {
+    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT) &&
+        IPCSkeleton::GetCallingUid() != ACCOUNT_UID) {
         return E_PERMISSION_DENIED;
     }
     int32_t userId = data.ReadInt32();
@@ -311,7 +312,8 @@ int32_t StorageManagerStub::HandlePrepareAddUser(MessageParcel &data, MessagePar
 
 int32_t StorageManagerStub::HandleRemoveUser(MessageParcel &data, MessageParcel &reply)
 {
-    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT)) {
+    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT) &&
+        IPCSkeleton::GetCallingUid() != ACCOUNT_UID) {
         return E_PERMISSION_DENIED;
     }
     int32_t userId = data.ReadInt32();
@@ -817,7 +819,8 @@ int32_t StorageManagerStub::HandleUpdateUserAuth(MessageParcel &data, MessagePar
 
 int32_t StorageManagerStub::HandleActiveUserKey(MessageParcel &data, MessageParcel &reply)
 {
-    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT)) {
+    if (!CheckClientPermissionForCrypt(PERMISSION_STORAGE_MANAGER_CRYPT) &&
+        IPCSkeleton::GetCallingUid() != ACCOUNT_UID) {
         return E_PERMISSION_DENIED;
     }
     uint32_t userId = data.ReadUint32();
