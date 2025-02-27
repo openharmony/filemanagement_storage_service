03a7271f9ce1872d408f701e21af2a616b953f74
hongshengquan <hongshengquan3@huawei.com>
2025-02-27 22:07:01 +0800

    Issue: [Bug]: 存储优先级调整 https://gitee.com/openharmony/filemanagement_storage_service/issues/IBPJB5 Description: 提升存储进程优先级 Feature or Bugfix: Feature Binary Source: No
    Signed-off-by: hongshengquan <hongshengquan3@huawei.com>

--- services/storage_daemon/include/ipc/storage_daemon.h
+++ services/storage_daemon/include/ipc/storage_daemon.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -147,6 +147,7 @@ private:
     int32_t RestoreconElX(uint32_t userId);
     void ActiveAppCloneUserKey();
     void SetDeleteFlag4KeyFiles();
+    void SetPriority();
 };
 } // StorageDaemon
 } // OHOS
