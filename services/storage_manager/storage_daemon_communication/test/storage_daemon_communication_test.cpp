7082918132e8ce18c476022dd6dfcb5c7d60bf3d
zhangchenyang <zhangchenyang30@huawei.com>
2025-03-06 14:01:38 +0800

    【master】【storage_service】文管fuse增加TDD
    Signed-off-by: zhangchenyang <zhangchenyang30@huawei.com>

--- services/storage_manager/storage_daemon_communication/test/storage_daemon_communication_test.cpp
+++ services/storage_manager/storage_daemon_communication/test/storage_daemon_communication_test.cpp
@@ -19,7 +19,6 @@
 #include "parameters.h"
 #include "storage_daemon_communication/storage_daemon_communication.h"
 #include "storage_service_errno.h"
-#include "test/common/help_utils.h"
 #include "user/multi_user_manager_service.h"
 
 namespace {
