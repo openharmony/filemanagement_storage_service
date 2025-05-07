816f13624f4ad0bf9c9e3d532d1f99a398741576
lixiyuan <lixiyuan7@huawei.com>
2025-05-07 10:31:45 +0800

    eventHandler
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- services/storage_daemon/include/crypto/crypto_delay_handler.h
+++ services/storage_daemon/include/crypto/crypto_delay_handler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,38 +13,41 @@
  * limitations under the License.
  */
 
-#ifndef STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
-#define STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
-
-#include <thread>
-
-#include "base_key.h"
-#include "event_handler.h"
-#include "libfscrypt/key_control.h"
-
-namespace OHOS {
-namespace StorageDaemon {
-class DelayHandler {
-// static const uint8_t CANCEL_CLEAR_ECE_SECE_TASK = 0;
-// static const uint8_t START_CLEAR_ECE_SECE_TASK = 1;
-public:
-    DelayHandler(uint32_t userId);
-    ~DelayHandler();
-    void StartDelayTask(std::shared_ptr<BaseKey> &el4Key);
-    void CancelDelayTask();
-
-private:
-    void StartDelayHandler();
-    void DeactiveEl3El4El5();
-
-    std::mutex eventMutex_;
-    std::thread eventThread_;
-    std::condition_variable eventCon_;
-    std::shared_ptr<AppExecFwk::EventHandler> eventHandler_ = nullptr;
-    std::shared_ptr<BaseKey> el4Key_;
-    uint32_t userId_;
-};
-} // StorageDaemon
-} // OHOS
-
-#endif // STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+ #ifndef STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+ #define STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+ 
+ #include <atomic>
+ #include <mutex>
+ #include <vector>
+ 
+ #include "timer.h"
+ 
+ #include "base_key.h"
+ #include "libfscrypt/key_control.h"
+ 
+ namespace OHOS {
+ namespace StorageDaemon {
+ class DelayHandler {
+ public:
+     DelayHandler(uint32_t userId);
+     ~DelayHandler();
+     void StartDelayTask(const std::shared_ptr<BaseKey> &el4Key);
+     void CancelDelayTask();
+ 
+ private:
+     uint32_t timerId_;
+     Utils::Timer timer_ {"DeativationEl3El4El5_Task_Timer"};
+ 
+     std::atomic<bool> cancelled_;
+     std::shared_ptr<BaseKey> el4Key_;
+     uint32_t userId_;
+ 
+     std::mutex handlerMutex_;
+ 
+     void DeactiveEl3El4El5();
+     int64_t GetTickCount();
+ };
+ } // StorageDaemon
+ } // OHOS
+ 
+ #endif // STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
