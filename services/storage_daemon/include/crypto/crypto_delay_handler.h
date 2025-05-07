a07d3c2bca6dd5d98b082a62c2b3173acab9b853
lixiyuan <lixiyuan7@huawei.com>
2025-05-07 10:41:59 +0800

    code check
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- services/storage_daemon/include/crypto/crypto_delay_handler.h
+++ services/storage_daemon/include/crypto/crypto_delay_handler.h
@@ -13,41 +13,41 @@
  * limitations under the License.
  */
 
- #ifndef STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
- #define STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
- 
- #include <atomic>
- #include <mutex>
- #include <vector>
- 
- #include "timer.h"
- 
- #include "base_key.h"
- #include "libfscrypt/key_control.h"
- 
- namespace OHOS {
- namespace StorageDaemon {
- class DelayHandler {
- public:
-     DelayHandler(uint32_t userId);
-     ~DelayHandler();
-     void StartDelayTask(const std::shared_ptr<BaseKey> &el4Key);
-     void CancelDelayTask();
- 
- private:
-     uint32_t timerId_;
-     Utils::Timer timer_ {"DeativationEl3El4El5_Task_Timer"};
- 
-     std::atomic<bool> cancelled_;
-     std::shared_ptr<BaseKey> el4Key_;
-     uint32_t userId_;
- 
-     std::mutex handlerMutex_;
- 
-     void DeactiveEl3El4El5();
-     int64_t GetTickCount();
- };
- } // StorageDaemon
- } // OHOS
- 
- #endif // STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+#ifndef STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+#define STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
+
+#include <atomic>
+#include <mutex>
+#include <vector>
+
+#include "timer.h"
+
+#include "base_key.h"
+#include "libfscrypt/key_control.h"
+
+namespace OHOS {
+namespace StorageDaemon {
+class DelayHandler {
+public:
+    DelayHandler(uint32_t userId);
+    ~DelayHandler();
+    void StartDelayTask(const std::shared_ptr<BaseKey> &el4Key);
+    void CancelDelayTask();
+
+private:
+    uint32_t timerId_;
+    Utils::Timer timer_ {"DeativationEl3El4El5_Task_Timer"};
+
+    std::atomic<bool> cancelled_;
+    std::shared_ptr<BaseKey> el4Key_;
+    uint32_t userId_;
+
+    std::mutex handlerMutex_;
+
+    void DeactiveEl3El4El5();
+    int64_t GetTickCount();
+};
+} // StorageDaemon
+} // OHOS
+
+#endif // STORAGE_DAEMON_CRYPTO_DELAY_HANDLER_H
