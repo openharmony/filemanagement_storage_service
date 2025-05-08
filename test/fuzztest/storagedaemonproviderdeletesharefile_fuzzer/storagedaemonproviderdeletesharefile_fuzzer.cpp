82408afecd2a9f1fb92de235ad44dee707bf2e45
BrainL <libuyan1@huawei.com>
2025-05-08 17:56:35 +0800

    存储文件管理daemon测试用例add
    Signed-off-by: BrainL <libuyan1@huawei.com>
    Change-Id: I3cfdca54b2a0ec742319c69d9416d2d9eb5a06ac

---
+++ test/fuzztest/storagedaemonproviderdeletesharefile_fuzzer/storagedaemonproviderdeletesharefile_fuzzer.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ #include "storagedaemonproviderdeletesharefile_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+
+#include "message_parcel.h"
+#include "storage_daemon_stub.h"
+#include "storage_daemon_provider.h"
+#include "securec.h"
+
+using namespace OHOS::StorageDaemon;
+
+namespace OHOS {
+
+std::shared_ptr<StorageDaemon::StorageDaemonProvider> storageDaemon =
+    std::make_shared<StorageDaemon::StorageDaemonProvider>();
+
+uint32_t GetU32Data(const char* ptr)
+{
+    // 将第0个数字左移24位，将第1个数字左移16位，将第2个数字左移8位，第3个数字不左移
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
+}
+
+bool StorageDaemonProviderDeleteShareFileFuzzTest(const uint8_t *data, size_t size)
+{
+    uint32_t code = static_cast<int32_t>(IStorageDaemonIpcCode::COMMAND_DELETE_SHARE_FILE);
+    MessageParcel datas;
+    datas.WriteInterfaceToken(StorageDaemon::StorageDaemonStub::GetDescriptor());
+    datas.WriteBuffer(data, size);
+    datas.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+    storageDaemon->OnRemoteRequest(code, datas, reply, option);
+
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    OHOS::StorageDaemonProviderDeleteShareFileFuzzTest(data, size);
+    return 0;
+}
