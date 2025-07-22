3728409f0abe7b967496d0b321d02eaa9a080a52
lixiyuan <lixiyuan7@huawei.com>
2025-07-22 10:45:48 +0800

    conflict fix
    Signed-off-by: lixiyuan <lixiyuan7@huawei.com>

--- services/storage_manager/volume/src/volume_manager_service.cpp
+++ services/storage_manager/volume/src/volume_manager_service.cpp
@@ -53,7 +53,7 @@ void VolumeManagerService::OnVolumeCreated(VolumeCore vc)
 void VolumeManagerService::OnVolumeStateChanged(string volumeId, VolumeState state)
 {
     if (state == VolumeState::FUSE_REMOVED) {
-        int32_t result = VolumeManagerServiceExt::GetInstance()->NotifyUsbFuseUmount(volumeId);
+        int32_t result = VolumeManagerServiceExt::getinstance().NotifyUsbFuseUmount(volumeId);
         if (result != E_OK) {
             LOGE("VolumeManagerServiceExt NotifyUsbFuseUmount failed, error = %{public}d", result);
         }
@@ -192,7 +192,7 @@ int32_t VolumeManagerService::MountUsbFuse(const std::string &volumeId)
     int32_t fuseFd;
     int32_t result = sdCommunication->MountUsbFuse(volumeId, fsUuid, fuseFd);
     if (result == E_OK) {
-        result = VolumeManagerServiceExt::GetInstance()->NotifyUsbFuseMount(fuseFd, volumeId, fsUuid);
+        result = VolumeManagerServiceExt::getinstance().NotifyUsbFuseMount(fuseFd, volumeId, fsUuid);
     }
     LOGI("VolumeManagerService::MountUsbFuse out");
     return result;
@@ -370,7 +370,7 @@ int32_t VolumeManagerService::Format(std::string volumeId, std::string fsType)
     }
 
     if (StorageDaemon::IsUsbFuse()) {
-        result = VolumeManagerServiceExt::GetInstance()->NotifyUsbFuseUmount(volumeId);
+        result = VolumeManagerServiceExt::getinstance().NotifyUsbFuseUmount(volumeId);
     }
     return result;
 }
