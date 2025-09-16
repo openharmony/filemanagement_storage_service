41a131078af5f7db9acf9a868fdbf5fab6367d22
faker8 <fengkun7@huawei.com>
2025-09-16 15:17:21 +0800

    修改shutdown文件名
    Signed-off-by: faker8 <fengkun7@huawei.com>

--- test/fuzztest/storagedaemonprovidershutdown_fuzzer/storagedaemonprovidershutdown_fuzzer.cpp
+++ test/fuzztest/storagedaemonprovidershutdown_fuzzer/storagedaemonprovidershutdown_fuzzer.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "storagedaemonproviderlockuserscreen_fuzzer.h"
+#include "storagedaemonprovidershutdown_fuzzer.h"
 
 #include <cstddef>
 #include <cstdint>
