f814172f7aec22a13b1685dd5881f4c688c6eddf
hongshengquan <hongshengquan3@huawei.com>
2025-09-29 22:28:56 +0800

    Issue: https://gitcode.com/openharmony/filemanagement_storage_service/issues/1781 Description: TestFuzz2 Feature or Bugfix: Bugfix Binary Source: No
    Signed-off-by: hongshengquan <hongshengquan3@huawei.com>

---
+++ test/fuzztest/storagemanagerproviderremoveuser_fuzzer/storagemanagerproviderremoveuser_fuzzer.cpp
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "storagemanagerproviderremoveuser_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <system_ability_definition.h>
+
+#include "accesstoken_kit.h"
+#include "ipc/storage_manager.h"
+#include "ipc/storage_manager_provider.h"
+#include "ipc_skeleton.h"
+#include "message_parcel.h"
+#include "securec.h"
+#include "storage_manager_stub.h"
+
+using namespace OHOS::StorageManager;
+
+namespace OHOS::Security::AccessToken {
+ATokenTypeEnum AccessTokenKit::GetTokenTypeFlag(AccessTokenID tokenID)
+{
+    return Security::AccessToken::TOKEN_NATIVE;
+}
+
+int AccessTokenKit::VerifyAccessToken(AccessTokenID tokenID, const std::string &permissionName)
+{
+    return Security::AccessToken::PermissionState::PERMISSION_GRANTED;
+}
+
+int AccessTokenKit::GetNativeTokenInfo(AccessTokenID tokenID, NativeTokenInfo &nativeTokenInfoRes)
+{
+    nativeTokenInfoRes.processName = "foundation";
+    return 0;
+}
+} // namespace OHOS::Security::AccessToken
+
+namespace OHOS {
+#ifdef CONFIG_IPC_SINGLE
+using namespace IPC_SINGLE;
+#endif
+pid_t IPCSkeleton::GetCallingUid()
+{
+    pid_t callingUid = 5523;
+    return callingUid;
+}
+
+uint32_t IPCSkeleton::GetCallingTokenID()
+{
+    uint32_t callingTokenID = 100;
+    return callingTokenID;
+}
+} // namespace OHOS
+
+namespace OHOS::StorageManager {
+constexpr uint8_t MAX_CALL_TRANSACTION = 64;
+constexpr size_t U32_AT_SIZE = 4;
+
+std::shared_ptr<StorageManagerProvider> storageManagerProvider =
+    std::make_shared<StorageManagerProvider>(STORAGE_MANAGER_MANAGER_ID);
+
+uint32_t GetU32Data(const char *ptr)
+{
+    // 将第0个数字左移24位，将第1个数字左移16位，将第2个数字左移8位，第3个数字不左移
+    return (ptr[0] << 24) | (ptr[1] << 16) | (ptr[2] << 8) | (ptr[3]);
+}
+
+bool g_storageManagerProviderAddUserFTest(std::unique_ptr<char[]> data, size_t size)
+{
+    uint32_t code = GetU32Data(data.get());
+    if (code == 0) {
+        return true;
+    }
+    MessageParcel datas;
+    datas.WriteInterfaceToken(StorageManagerStub::GetDescriptor());
+    datas.WriteBuffer(data.get(), size);
+    datas.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+    storageManagerProvider->OnRemoteRequest(code % MAX_CALL_TRANSACTION, datas, reply, option);
+
+    return true;
+}
+
+bool RemoveUserFuzzTest(const uint8_t *data, size_t size)
+{
+    uint32_t code = static_cast<uint32_t>(IStorageManagerIpcCode::COMMAND_REMOVE_USER);
+    MessageParcel datas;
+    datas.WriteInterfaceToken(StorageManagerStub::GetDescriptor());
+    datas.WriteBuffer(data, size);
+    datas.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+
+    storageManagerProvider->OnRemoteRequest(code, datas, reply, option);
+    return true;
+}
+} // namespace OHOS::StorageManager
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size < OHOS::StorageManager::U32_AT_SIZE) {
+        return 0;
+    }
+
+    auto str = std::make_unique<char[]>(size + 1);
+    (void)memset_s(str.get(), size + 1, 0x00, size + 1);
+    if (memcpy_s(str.get(), size, data, size) != EOK) {
+        return 0;
+    }
+
+    OHOS::StorageManager::g_storageManagerProviderAddUserFTest(move(str), size);
+    OHOS::StorageManager::RemoveUserFuzzTest(data, size);
+    return 0;
+}
