6a05d86d3be2285bb99ca01a7ad07577f023e5bf
li-zhangkai <lizhangkai@h-partners.com>
2025-10-15 10:41:10 +0800

    fix
    Signed-off-by: li-zhangkai <lizhangkai@h-partners.com>

--- services/storage_daemon/user/test/mount_manager_test.cpp
+++ services/storage_daemon/user/test/mount_manager_test.cpp
@@ -987,6 +987,11 @@ HWTEST_F(MountManagerTest, Storage_Daemon_MountManagerExtTest_MountDisShareFile_
     EXPECT_EQ(ret, E_NON_EXIST);
 
     EXPECT_CALL(*fileUtilMoc_, IsDir(_)).WillOnce(Return(true)).WillOnce(Return(true));
+    EXPECT_CALL(*fileUtilMoc_, MkDirRecurse(_, _)).WillOnce(Return(false));
+    ret = MountManager::GetInstance().MountDisShareFile(userId, shareFiles);
+    EXPECT_EQ(ret, E_NON_EXIST);
+ 
+    EXPECT_CALL(*fileUtilMoc_, IsDir(_)).WillOnce(Return(true)).WillOnce(Return(true));
     EXPECT_CALL(*fileUtilMoc_, Mount(_, _, _, _, _)).WillOnce(Return(1));
     ret = MountManager::GetInstance().MountDisShareFile(userId, shareFiles);
     EXPECT_EQ(ret, E_MOUNT_SHARE_FILE);
