d2f28351e6bad1da7b8a8b35a694d6bac9f83818
faker8 <fengkun7@huawei.com>
2025-10-28 16:39:55 +0800

    首次补充fuzz
    Signed-off-by: faker8 <fengkun7@huawei.com>

--- test/fuzztest/keycontrolgetfscryptversion_fuzzer/keycontrolgetfscryptversion_fuzzer.cpp
+++ test/fuzztest/keycontrolgetfscryptversion_fuzzer/keycontrolgetfscryptversion_fuzzer.cpp
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#include "keycontrol_fuzzer.h"
+#include "keycontrolgetfscryptversion_fuzzer.h"
 #include "key_control.h"
 #include <securec.h>
 #include <cstddef>
@@ -26,28 +26,13 @@ bool SysparamDynamicFuzzTest(const uint8_t *data, size_t size)
     if ((data == nullptr) || (size < sizeof(int))) {
         return false;
     }
-    union FscryptPolicy policy1;
-    union FscryptPolicy *policy = &policy1;
+    int state = *(reinterpret_cast<const int *>(data));
     char character[MAX_NUM] = { 0x00 };
     if (EOK != memcpy_s(character, sizeof(character)-1, data, size)) {
         return false;
     }
 
-#ifdef SUPPORT_FSCRYPT_V2
-    struct fscrypt_add_key_arg fscryptaddkeyarg;
-    struct fscrypt_add_key_arg *fscryptaddkeyarg2 = &fscryptaddkeyarg;
-    struct fscrypt_remove_key_arg fscryptremovekeyarg;
-    struct fscrypt_remove_key_arg *fscryptremovekeyarg2 = &fscryptremovekeyarg;
-    struct fscrypt_get_key_status_arg fscryptgetkeystatusarg;
-    struct fscrypt_get_key_status_arg *fscryptgetkeystatusarg2 = &fscryptgetkeystatusarg;
-    struct fscrypt_get_policy_ex_arg fscryptgetpolicyexarg;
-    struct fscrypt_get_policy_ex_arg *fscryptgetpolicyexarg2 = &fscryptgetpolicyexarg;
-    KeyCtrlInstallKey(character, fscryptaddkeyarg2);
-    KeyCtrlRemoveKey(character, fscryptremovekeyarg2);
-    KeyCtrlGetKeyStatus(character, fscryptgetkeystatusarg2);
-    KeyCtrlGetPolicyEx(character, fscryptgetpolicyexarg2);
-#endif
-    KeyCtrlSetPolicy(character, policy);
+    KeyCtrlGetFscryptVersion(character);
     return true;
 }
 } // namespace OHOS
