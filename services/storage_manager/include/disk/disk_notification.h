b0173135ac1969caf032fbe00b186ce564339106
Summer_58 <wangchangsheng9@h-partners.com>
2026-01-30 16:07:33 +0800

    update: 更新文件 disk_notification.h
    Signed-off-by: Summer_58 <wangchangsheng9@h-partners.com>

--- services/storage_manager/include/disk/disk_notification.h
+++ services/storage_manager/include/disk/disk_notification.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,4 +40,4 @@ private:
 } // StorageManager
 } // OHOS
 
-#endif // OHOS_STORAGE_MANAGER_DISKNOTIFICATION_H
+#endif // OHOS_STORAGE_MANAGER_DISKNOTIFICATION_H
