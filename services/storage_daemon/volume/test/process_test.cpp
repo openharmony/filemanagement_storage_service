fa4b60acf987fb584c65b5a2a5a98b4361965c80
zhangchenyang <zhangchenyang30@h-partners.com>
2026-02-11 16:02:18 +0800

    【6.0】【storage】漏洞修复
    Signed-off-by: zhangchenyang <zhangchenyang30@h-partners.com>

--- services/storage_daemon/volume/test/process_test.cpp
+++ services/storage_daemon/volume/test/process_test.cpp
@@ -36,75 +36,40 @@ public:
 };
 
 /**
- * @tc.name: Storage_Service_ProcessTest_CheckSubDir_001
- * @tc.desc: Verify the CheckSubDir function.
+ * @tc.name: Storage_Service_ProcessTest_GetPath_001
+ * @tc.desc: Verify the GetPath function.
  * @tc.type: FUNC
  * @tc.require: SR000GGUOT
  */
-HWTEST_F(ProcessTest, Storage_Service_ProcessTest_CheckSubDir_001, TestSize.Level1)
+HWTEST_F(ProcessTest, Storage_Service_ProcessTest_GetXXX_001, TestSize.Level1)
 {
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_CheckSubDir_001 start";
-    Process process1("/usr/local/bin");
-    EXPECT_FALSE(process1.CheckSubDir("/usr/local"));
+    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_GetXXX_001 start";
 
-    Process process2("/usr/local/bin");
-    EXPECT_FALSE(process2.CheckSubDir("/usr/local/"));
+    std::string volId = "vol-1-1";
+    std::string diskId = "disk-1-1";
+    bool isUserdata = false;
+    dev_t device = MKDEV(1, 1); // 1 is major device number, 1 is minor device number
+    uint32_t mountFlags = 0;
+    ExternalVolumeInfoMock mock;
+    EXPECT_CALL(mock, DoCreate(testing::_)).Times(1).WillOnce(testing::Return(E_OK));
+    EXPECT_CALL(mock, DoCheck()).Times(1).WillOnce(testing::Return(E_OK));
+    EXPECT_CALL(mock, DoMount(testing::_)).Times(1).WillOnce(testing::Return(E_OK));
 
-    Process process3("/usr/local/bin");
-    EXPECT_FALSE(process3.CheckSubDir("/usr/locala"));
-
-    Process process4("/usr/local/bin");
-    EXPECT_TRUE(process4.CheckSubDir("/usr/local/bin/extra"));
-
-    Process process5("/usr/local/bin");
-    EXPECT_FALSE(process5.CheckSubDir("/usr/local/b"));
-
-    Process process6("/usr/local/bin");
-    EXPECT_FALSE(process6.CheckSubDir(""));
-
-    Process process7("/usr/local/bin");
-    EXPECT_TRUE(process7.CheckSubDir("/usr/local/bin/"));
-
-    Process process8("/usr/local/bin");
-    EXPECT_TRUE(process8.CheckSubDir("/usr/local/bin"));
-
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_CheckSubDir_001 end";
-}
-
-/**
- * @tc.name: Storage_Service_ProcessTest_CheckFds_001
- * @tc.desc: Verify the CheckFds function.
- * @tc.type: FUNC
- * @tc.require: SR000GGUOT
- */
-HWTEST_F(ProcessTest, Storage_Service_ProcessTest_CheckFds_001, TestSize.Level1)
-{
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_CheckFds_001 start";
-    Process process1("/path/to/process");
-    EXPECT_FALSE(process1.CheckFds("/proc/1"));
-
-    Process process2("/path/to/process");
-    EXPECT_FALSE(process2.CheckFds("/proc/111111"));
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_CheckFds_001 end";
-}
-
-/**
- * @tc.name: Storage_Service_ProcessTest_UpdatePidAndKill_001
- * @tc.desc: Verify the UpdatePidAndKill function to fix TOCTOU vulnerability.
- * @tc.type: FUNC
- * @tc.require: SR000GGUOT
- */
-HWTEST_F(ProcessTest, Storage_Service_ProcessTest_UpdatePidAndKill_001, TestSize.Level1)
-{
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_UpdatePidAndKill_001 start";
-    Process process1("/path/to/process");
-    auto ret = process1.UpdatePidAndKill(0);
+    auto ret = mock.Create(volId, diskId, device, isUserdata);
     EXPECT_TRUE(ret == E_OK);
 
-    Process process2("/path/to/process");
-    ret = process2.UpdatePidAndKill(SIGKILL);
+    ret = mock.Check();
     EXPECT_TRUE(ret == E_OK);
-    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_UpdatePidAndKill_001 end";
+
+    ret = mock.Mount(mountFlags);
+    EXPECT_TRUE(ret == E_OK);
+
+    auto mountPath = mock.GetMountPath();
+    Process ps(mountPath);
+    ps.UpdatePidAndKill();
+    EXPECT_TRUE(ps.GetPath() == mountPath);
+
+    GTEST_LOG_(INFO) << "Storage_Service_ProcessTest_GetXXX_001 end";
 }
 } // STORAGE_DAEMON
 } // OHOS
