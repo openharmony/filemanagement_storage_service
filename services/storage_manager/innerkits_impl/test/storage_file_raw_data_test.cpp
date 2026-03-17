a97ec0bfd87ca05392d38405eb2a00f1d5159662
yang-li86 <yangli228@h-partners.com>
2026-03-17 16:21:01 +0800

    test: add unit tests for storage manager
    - Add unit tests for StorageFileRawData::RawDataCpy method covering all code branches
    - Add unit tests for NextDqBlk and DirSpaceInfo marshalling/unmarshalling
    - Add unit tests for StorageQuotaController::SaveUidAndBaseLine with various edge cases
    - Optimize test cases by removing redundant tests, keeping only essential branch coverage
    
    Signed-off-by: yang-li86 <yangli228@h-partners.com>
    Co-Authored-By: Agent

--- services/storage_manager/innerkits_impl/test/storage_file_raw_data_test.cpp
+++ services/storage_manager/innerkits_impl/test/storage_file_raw_data_test.cpp
@@ -14,6 +14,7 @@
  */
 
 #include <cstdio>
+#include <cstring>
 #include <gtest/gtest.h>
 
 #include "storage_file_raw_data.h"
@@ -30,6 +31,8 @@ public:
     void SetUp() {};
     void TearDown() {};
 };
+
+// ==================== RawDataCpy Tests ====================
 
 /**
  * @tc.number: SUB_STORAGE_FILE_RAW_DATA_RawDataCpy_0000
@@ -60,4 +63,254 @@ HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_RawDataCpy_0000, testing:
 
     GTEST_LOG_(INFO) << "StorageFileRawDataTest-end Storage_File_Raw_Data_RawDataCpy_0000";
 }
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_RawDataCpy_0001
+ * @tc.name: Storage_File_Raw_Data_RawDataCpy_0001
+ * @tc.desc: Test RawDataCpy with negative size.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_RawDataCpy_0001, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0001 start";
+
+    StorageFileRawData storage;
+    char data[10] = "test";
+    storage.size = -1;
+    EXPECT_EQ(storage.RawDataCpy(data), ERR_INVALID_DATA);
+    EXPECT_EQ(storage.data, nullptr);
+    EXPECT_FALSE(storage.isMalloc);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0001 end";
+}
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_RawDataCpy_0002
+ * @tc.name: Storage_File_Raw_Data_RawDataCpy_0002
+ * @tc.desc: Test RawDataCpy replacing existing data.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_RawDataCpy_0002, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0002 start";
+
+    StorageFileRawData storage;
+    char data1[10] = "first";
+    storage.size = 10;
+    EXPECT_EQ(storage.RawDataCpy(data1), ERR_NONE);
+    void* firstPtr = const_cast<void*>(storage.data);
+    EXPECT_NE(firstPtr, nullptr);
+
+    // Replace with new data
+    char data2[20] = "second_data_larger";
+    storage.size = 20;
+    EXPECT_EQ(storage.RawDataCpy(data2), ERR_NONE);
+    void* secondPtr = const_cast<void*>(storage.data);
+    EXPECT_NE(secondPtr, nullptr);
+    EXPECT_NE(firstPtr, secondPtr);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0002 end";
+}
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_RawDataCpy_0003
+ * @tc.name: Storage_File_Raw_Data_RawDataCpy_0003
+ * @tc.desc: Test RawDataCpy with size at MAX boundary.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_RawDataCpy_0003, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0003 start";
+
+    StorageFileRawData storage;
+    char data[1024];
+    memset(data, 'X', 1024);
+
+    // Test with size one less than MAX
+    storage.size = StorageFileRawData::MAX_RAW_DATA_SIZE - 1;
+    EXPECT_EQ(storage.RawDataCpy(data), ERR_INVALID_DATA);
+
+    // Test with size equal to MAX (should fail)
+    storage.size = StorageFileRawData::MAX_RAW_DATA_SIZE;
+    EXPECT_EQ(storage.RawDataCpy(data), ERR_INVALID_DATA);
+
+    // Test with size greater than MAX
+    storage.size = StorageFileRawData::MAX_RAW_DATA_SIZE + 1;
+    EXPECT_EQ(storage.RawDataCpy(data), ERR_INVALID_DATA);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_RawDataCpy_0003 end";
+}
+
+// ==================== Destructor Tests ====================
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_Destructor_0001
+ * @tc.name: Storage_File_Raw_Data_Destructor_0001
+ * @tc.desc: Test destructor with no data allocated.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_Destructor_0001, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_Destructor_0001 start";
+
+    StorageFileRawData* storage = new StorageFileRawData();
+    storage->data = nullptr;
+    storage->size = 0;
+    storage->isMalloc = false;
+    delete storage;
+    EXPECT_TRUE(true);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_Destructor_0001 end";
+}
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_Destructor_0002
+ * @tc.name: Storage_File_Raw_Data_Destructor_0002
+ * @tc.desc: Test destructor with allocated data.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_Destructor_0002, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_Destructor_0002 start";
+
+    StorageFileRawData* storage = new StorageFileRawData();
+    char data[100] = "test_data_for_destructor";
+    storage->size = 100;
+    storage->RawDataCpy(data);
+    EXPECT_NE(storage->data, nullptr);
+    EXPECT_TRUE(storage->isMalloc);
+
+    // Destructor should free allocated memory
+    delete storage;
+    EXPECT_TRUE(true);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_Destructor_0002 end";
+}
+
+// ==================== InitialState Tests ====================
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_InitialState_0001
+ * @tc.name: Storage_File_Raw_Data_InitialState_0001
+ * @tc.desc: Test initial state of StorageFileRawData.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_InitialState_0001, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_InitialState_0001 start";
+
+    StorageFileRawData storage;
+    EXPECT_EQ(storage.data, nullptr);
+    EXPECT_EQ(storage.size, 0);
+    EXPECT_FALSE(storage.isMalloc);
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_InitialState_0001 end";
+}
+
+// ==================== MultipleCopies Tests ====================
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_MultipleCopies_0001
+ * @tc.name: Storage_File_Raw_Data_MultipleCopies_0001
+ * @tc.desc: Test creating multiple objects and copying data.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_MultipleCopies_0001, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0001 start";
+
+    const int count = 10;
+    StorageFileRawData storages[count];
+
+    for (int i = 0; i < count; i++) {
+        char data[64];
+        sprintf_s(data, sizeof(data), "data_%d", i);
+        storages[i].size = static_cast<uint32_t>(strlen(data) + 1);
+        EXPECT_EQ(storages[i].RawDataCpy(data), ERR_NONE);
+        EXPECT_NE(storages[i].data, nullptr);
+        EXPECT_TRUE(storages[i].isMalloc);
+    }
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0001 end";
+}
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_MultipleCopies_0002
+ * @tc.name: Storage_File_Raw_Data_MultipleCopies_0002
+ * @tc.desc: Test multiple objects with different sizes.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_MultipleCopies_0002, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0002 start";
+
+    uint32_t sizes[] = {10, 50, 100, 500, 1000};
+    const int count = sizeof(sizes) / sizeof(sizes[0]);
+
+    for (int i = 0; i < count; i++) {
+        StorageFileRawData storage;
+        char* data = new char[sizes[i]];
+        memset(data, 'A' + i, sizes[i]);
+        storage.size = sizes[i];
+        EXPECT_EQ(storage.RawDataCpy(data), ERR_NONE);
+        EXPECT_NE(storage.data, nullptr);
+        EXPECT_TRUE(storage.isMalloc);
+        delete[] data;
+    }
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0002 end";
+}
+
+/**
+ * @tc.number: SUB_STORAGE_FILE_RAW_DATA_MultipleCopies_0003
+ * @tc.name: Storage_File_Raw_Data_MultipleCopies_0003
+ * @tc.desc: Test sequential copies with increasing sizes.
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000GGUPG
+ */
+HWTEST_F(StorageFileRawDataTest, Storage_File_Raw_Data_MultipleCopies_0003, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0003 start";
+
+    StorageFileRawData storage;
+
+    for (uint32_t size = 10; size <= 100; size += 10) {
+        char* data = new char[size];
+        memset(data, 'X', size);
+        storage.size = size;
+        EXPECT_EQ(storage.RawDataCpy(data), ERR_NONE);
+        EXPECT_NE(storage.data, nullptr);
+        EXPECT_TRUE(storage.isMalloc);
+        delete[] data;
+    }
+
+    GTEST_LOG_(INFO) << "Storage_File_Raw_Data_MultipleCopies_0003 end";
+}
+
 }
