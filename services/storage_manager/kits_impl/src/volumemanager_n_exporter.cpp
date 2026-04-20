9f2fa79b747cc0fdb4e8c1c63007db8db6860ca7
gemingjia <571727628@qq.com>
2026-04-20 14:01:58 +0800

    OpBurn
    Signed-off-by: gemingjia <571727628@qq.com>

--- services/storage_manager/kits_impl/src/volumemanager_n_exporter.cpp
+++ services/storage_manager/kits_impl/src/volumemanager_n_exporter.cpp
@@ -543,7 +543,7 @@ napi_value GetOpticalDriveOpsProgress(napi_env env, napi_callback_info info)
         if (err) {
             return {env, err.GetNapiErr(env)};
         }
-        return NVal::CreateUndefined(env);
+        return NVal::CreateUndefined(env, *progress);
     };
 
     std::string procedureName = "GetOpticalDriveOpsProgress";
