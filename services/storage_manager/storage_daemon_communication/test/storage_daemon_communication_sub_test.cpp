4bc7d4e0dbcbc7f32c332444f01d04a77446d03c
gzhuangzhuang <gengzhuangzhuang@huawei.com>
2026-06-17 18:08:35 +0800

    fix: restore AddDeathRecipient lambda capture in MockAllSuccess
    Co-Authored-By: Agent
    Signed-off-by: gzhuangzhuang <gengzhuangzhuang@huawei.com>
    Change-Id: Ic6b026a38f1de3e06e1ba2bb41a0ed4abf69c631

--- services/storage_manager/storage_daemon_communication/test/storage_daemon_communication_sub_test.cpp
+++ services/storage_manager/storage_daemon_communication/test/storage_daemon_communication_sub_test.cpp
@@ -143,7 +143,9 @@ void StorageDaemonCommunicationTest::MockAllSuccess()
     sdCommunication.ResetSdProxy();
     EXPECT_CALL(*sa, GetSystemAbilityManager()).Times(AnyNumber()).WillRepeatedly(Return(sam));
     EXPECT_CALL(*sam, GetSystemAbility(_)).Times(AnyNumber()).WillRepeatedly(Return(sd));
-    EXPECT_CALL(*sd, AddDeathRecipient(_)).WillOnce(Return(true));
+    EXPECT_CALL(*sd, AddDeathRecipient(_)).WillOnce(DoAll(Invoke([&sdCommunication {sdCommunication}] () {	 
+         sdCommunication.storageDaemon_ = nullptr; 
+     }), Return(true)));
 }
 
 /**
