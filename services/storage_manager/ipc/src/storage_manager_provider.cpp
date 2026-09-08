9c23279065524a53223c1d3be8cc0b772b562148
cuiruibin <cuiruibin3@h-partners.com>
2026-09-08 11:17:18 +0800

    refactor: merge CreateUserDir report to outer layer
    Remove duplicate ReportFucBehavior from UserManager::CreateUserDir (inner layer). Consolidate all info (path, mode, uid, gid, callingUid) into StorageManagerProvider::CreateUserDir (outer layer).
    
    Co-Authored-By: Agent
    Signed-off-by: cuiruibin <cuiruibin3@h-partners.com>
    Change-Id: I4e4dccd869613dca7fd8dbe2cb8cf8aaa1525eb6

--- services/storage_manager/ipc/src/storage_manager_provider.cpp
+++ services/storage_manager/ipc/src/storage_manager_provider.cpp
@@ -1630,7 +1630,9 @@ int32_t StorageManagerProvider::CreateUserDir(const std::string &path, mode_t mo
     auto ret = sdCommunication.CreateUserDir(path, mode, uid, gid);
     LOGW("CreateUserDir end, uid: %{public}d, ret: %{public}d", callingUid, ret);
 
-    std::string extraData = "path=" + path + "callingUid=" + std::to_string(callingUid);
+    std::string extraData = "path=" + path + ", mode=" + std::to_string(mode) +
+        ", uid=" + std::to_string(uid) + ", gid=" + std::to_string(gid) +
+        ", callingUid=" + std::to_string(callingUid);
     StorageRadar::ReportFucBehavior("CreateUserDir", 0, extraData, ret);
     return ret;
 }
