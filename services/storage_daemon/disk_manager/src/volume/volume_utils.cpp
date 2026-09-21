629875ec5da04d5c8e46debaeacbdc4be9331d5d
zhonglufu <zhonglufu@h-partners.com>
2026-09-21 11:36:53 +0800

    update: 更新文件 volume_utils.cpp
    Signed-off-by: zhonglufu <zhonglufu@h-partners.com>

--- services/storage_daemon/disk_manager/src/volume/volume_utils.cpp
+++ services/storage_daemon/disk_manager/src/volume/volume_utils.cpp
@@ -35,8 +35,6 @@ namespace OHOS {
 namespace StorageDaemon {
 
 constexpr const char *MOUNT_PATH_PREFIX = "/mnt/data/";
-#define FDSAN_TAG 1
-const uint64_t NEW_TAG = static_cast<uint64_t>(0xD004301) << 32 | FDSAN_TAG;
 
 static bool IsValidMountPath(const std::string& mountPath)
 {
@@ -156,7 +154,6 @@ int32_t VolumeUtils::MountFuseDevice(const std::string& mountPath,
         LOGE("VolumeUtils::MountFuseDevice open /dev/fuse failed, errno=%{public}d", errno);
         return E_OPEN_FAILED;
     }
-    fdsan_exchange_owner_tag(fuseFd, 0, NEW_TAG);
     std::string fuseOptions = StringPrintf(
         "fd=%d,"
         "rootmode=40000,"
@@ -170,7 +167,6 @@ int32_t VolumeUtils::MountFuseDevice(const std::string& mountPath,
                      fuseOptions.c_str());
     if (mret != 0) {
         LOGE("VolumeUtils::MountFuseDevice mount failed, errno=%{public}d", errno);
-        fdsan_close_with_tag(fuseFd, NEW_TAG);
         fuseFd = -1;
         return E_EXT_MOUNT;
     }
